86d33975fd3297cb09f6ae2db304bf6a73d47b38
roeuihyun <roeuihyun@gmail.com>
2022-09-01 20:13:53 +0900

    git init

---
+++ withfutureEducation/src/main/java/com/roeuihyun/withfutureEducation/WithfutureEducationApplication.java
@@ -0,0 +1,13 @@
+package com.roeuihyun.withfutureEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithfutureEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithfutureEducationApplication.class, args);
+	}
+
+}
---
+++ withfutureEducation/src/test/java/com/roeuihyun/withfutureEducation/WithfutureEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roeuihyun.withfutureEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithfutureEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
